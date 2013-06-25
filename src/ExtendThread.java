67a6cec37312c3346f1bae57cc40e9c0937b2fcd
philipz <philipzheng@gmail.com>
2013-06-25 09:46:06 +0800

    First init

---
+++ src/ExtendThread.java
@@ -0,0 +1,67 @@
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
+import org.rosuda.REngine.JRI.JRIEngine;
+
+// Create a second thread by extending Thread
+class NewThread extends Thread {
+	JRIEngine re;
+	REXP rexp;
+	String name;
+
+	NewThread(String input) {
+		// Create a new, second thread
+		super("Demo Thread");
+		name = input;
+		System.out.println("Child thread" + name + ": " + this);
+		try {
+			re = new JRIEngine(new String[] { "--vanilla" });
+			// System.out.println("Rengine created, waiting for R");
+		} catch (REngineException e) {
+			e.printStackTrace();
+		}
+		start(); // Start the thread
+	}
+
+	// This is the entry point for the second thread.
+	public void run() {
+		try {
+			for (int i = 5; i > 0; i--) {
+				//System.out.println("Child Thread: " + i);
+				// Let the thread sleep for a while.
+				String tmp = i + " + " + i;
+				try {
+					rexp = re.parseAndEval(tmp);
+					int sum = rexp.asInteger();
+					System.out.println("Child Thread" + name + " i+i: " + sum);
+					re.close();
+				} catch (REngineException e) {
+					e.printStackTrace();
+				} catch (REXPMismatchException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Thread.sleep(500);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Child interrupted.");
+		}
+		System.out.println("Exiting child thread.");
+	}
+}
+
+public class ExtendThread {
+	public static void main(String args[]) {
+		new NewThread("1"); // create a new thread
+		new NewThread("2");
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Main Thread: " + (i + i));
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Main thread interrupted.");
+		}
+		System.out.println("Main thread exiting.");
+	}
+}
